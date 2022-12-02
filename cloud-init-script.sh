b37208246cdbb69e4894dbe80695ac6609d4f5a9
bandkarrajesh <64117250+bandkarrajesh@users.noreply.github.com>
2022-12-02 22:23:26 +0530

    Update cloud-init-script.sh

--- cloud-init-script.sh
+++ cloud-init-script.sh
@@ -2,3 +2,4 @@
 systemctl enable docker
 systemctl start docker
 systemctl stop firewalld
+docker swarm join --token SWMTKN-1-5ts2afn41f254mkergbhmrx8294pi8oqz5goxj7qodq9v7usuj-21s5h5ndx4dydx9tbtodbte9e 10.0.0.33:2377
